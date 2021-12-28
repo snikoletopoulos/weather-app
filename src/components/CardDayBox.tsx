85f7afaaf222b18e7d33448ab3a23518e07ea0c4
Stavros <snikoletopoulos@hotmail.com>
2021-12-28 22:08:38 +0200

    Delete old files

--- src/components/CardDayBox.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function CardDayBox() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
--- src/components/WeatherCard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function WeatherCard() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
