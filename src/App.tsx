476d0ee61ea24ffc0b6593d9f0a017b99c363056
Stavros <snikoletopoulos@hotmail.com>
2021-12-29 21:49:14 +0200

    Formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
-import UserInput from "components/selectedDay/UserInput";
-import DetailCard from "components/selectedDay/DetailCard";
-import { ICountryForecast } from "types/api-types";
 import styles from "./App.module.css";
 import "global.css";
+
+import { ICountryForecast } from "types/api-types";
+
+import Card from "components/UI/Card";
+import DayCards from "components/detailArea/DayCards";
+import UserInput from "components/selectedDay/UserInput";
+import DetailCard from "components/selectedDay/DetailCard";
 
 const App = () => {
   const [city, setCity] = useState<string>("");
