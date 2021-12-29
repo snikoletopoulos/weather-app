1c773c75e587815064e1b956237aea653d566464
Stavros <snikoletopoulos@hotmail.com>
2021-12-29 22:59:36 +0200

    Add http protocol and remove console.logs

--- src/logic.ts
+++ src/logic.ts
@@ -3,9 +3,8 @@ import { ICountryForecast } from "./types/api-types";
 
 export const getCityData: cityData = async city => {
   const response = await axios.get<ICountryForecast>(
-    `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}`
   );
-  console.log(response);
 
   return response.data;
 };
