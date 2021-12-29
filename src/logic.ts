9d29ef85f833f8716972bf1923d13fa4932d6a10
Stavros <snikoletopoulos@hotmail.com>
2021-12-29 22:36:32 +0200

    Search for city with city name

--- src/logic.ts
+++ src/logic.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 import { ICountryForecast } from "./types/api-types";
 
-export const getCityData: cityData = async cityId => {
+export const getCityData: cityData = async city => {
   const response = await axios.get<ICountryForecast>(
-    `api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${process.env.REACT_APP_API_KEY}`
+    `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}`
   );
   console.log(response);
 
   return response.data;
 };
 
-type cityData = (cityId: number) => Promise<ICountryForecast>;
+type cityData = (cityId: string) => Promise<ICountryForecast>;
