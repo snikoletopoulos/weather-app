67c305b1959fc0d2063b09a31b52b3074563827b
Stavros <snikoletopoulos@hotmail.com>
2022-07-24 23:36:45 +0300

    Add content to details section

--- src/components/selectedDay/DetailCard.tsx
+++ src/components/selectedDay/DetailCard.tsx
@@ -1,20 +1,37 @@
-const DetailCard = () => {
+import styles from "./DetailCard.module.css";
+
+import Icon from "@/components/UI/Icon";
+import Attribute from "@/components/selectedDay/Attribute";
+
+interface Props {
+	forecast: number;
+}
+
+const DetailCard: React.FC<Props> = ({ forecast }) => {
 	const today = new Date();
 
 	return (
-		<section>
+		<section className={styles["details-container"]}>
 			<p>
-				{today.toLocaleDateString("en-US", {
+				{today.toLocaleString("en-EN", {
 					hour: "numeric",
 					minute: "numeric",
 					hour12: true,
+				})}{" "}
+				{today.toLocaleDateString("en-US", {
 					weekday: "short",
 					year: "numeric",
 					month: "short",
 					day: "numeric",
 				})}
 			</p>
-			<h2></h2>
+			<Icon icon="10d" />
+			<h2>{forecast}</h2>
+			<h3>Cloudy</h3>
+			<div className={styles["attributes-container"]}>
+				<Attribute attribute={{ title: "Humidity", value: `45%` }} />
+				<Attribute attribute={{ title: "Wind Speed", value: `19.2 km/j` }} />
+			</div>
 		</section>
 	);
 };
